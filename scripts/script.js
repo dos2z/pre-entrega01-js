2919656cced4366fa434aae9e2bbddbaebe6da56
dos2z <eloy.zaid@gmail.com>
2023-11-25 18:59:01 -0300

    agrego metodo .toLowerCase() a los prompt

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 let pregunta;
 let rta;
-let resultado = 0;
+let resultado;
 
 
 function devolucion() {
@@ -14,25 +14,27 @@ function devolucion() {
 }
 
 alert("Responda ingresando 'a' , 'b' o 'c', para pasar debe superar el 70%; si no, debera repetir el cuestionario")
+
 while (resultado < 70) {
+
     resultado = 0
-    pregunta = prompt("¿Cuánto tarda la luz del sol en llegar a la tierra? a) 8 minutos;  b) 1 Hora  c) 13 minutos");
+    pregunta = prompt("¿Cuánto tarda la luz del sol en llegar a la tierra? a) 8 minutos;  b) 1 Hora  c) 13 minutos").toLowerCase();
     rta = pregunta == "a"
     devolucion()
 
-    pregunta = prompt("¿Cuál es el elemento 1 de la tabla periódica? a) Helio;  b) Hidrógeno;  c) Boro");
+    pregunta = prompt("¿Cuál es el elemento 1 de la tabla periódica? a) Helio;  b) Hidrógeno;  c) Boro").toLowerCase();
     rta = pregunta == "b"
     devolucion()
 
-    pregunta = prompt("¿Cuál es más alto? a) Aconcagua;  b) Kilimanjaro;  c) Everest");
+    pregunta = prompt("¿Cuál es más alto? a) Aconcagua;  b) Kilimanjaro;  c) Everest").toLowerCase();
     rta = pregunta == "c"
     devolucion()
 
-    pregunta = prompt("La melodia del feliz cumpleaños es igual a:  a) Arroz con leche; b) Payaso PlimPlim;  c) Mambrú se fue a la guerra");
+    pregunta = prompt("La melodia del feliz cumpleaños es igual a:  a) Arroz con leche; b) Payaso PlimPlim;  c) Mambrú se fue a la guerra").toLowerCase();
     rta = pregunta == "b"
     devolucion()
 
-    pregunta = prompt("La identidad secreta de Batman es: a) Bruno Diaz;  b) Clark Kent;  c) Oliver Twist");
+    pregunta = prompt("La identidad secreta de Batman es: a) Bruno Diaz;  b) Clark Kent;  c) Oliver Twist").toLowerCase();
     rta = pregunta == "a"
     devolucion()
 
